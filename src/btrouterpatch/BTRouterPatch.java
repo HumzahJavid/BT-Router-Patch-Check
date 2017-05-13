2c97b67c434735c529839a4dee5e224ff859de6a
HumzahJavid <HumzahJavid@users.noreply.github.com>
2017-05-13 14:22:12 +0100

    Check for duplicates combined, shifting towards arraylist
    Combined check for duplicates hostname and ip address into one function.
    Filename entered by user.
    The remaining invalid routers (with OS < 12 and prepatched) are dealt
    with using a cleaner method (and array lists)

--- src/btrouterpatch/BTRouterPatch.java
+++ src/btrouterpatch/BTRouterPatch.java
@@ -1,203 +1,183 @@
 package btrouterpatch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BTRouterPatch {
 
     public static void main(String[] args) throws Exception {
-        java.io.File file = new java.io.File("sample.csv");
-        Scanner input = new Scanner(file);
-        boolean isDuplicateHost = false;
-        boolean isDuplicateIP = false;
-        boolean patched = false;
-        boolean invalidOS = false;
-        String currentLine = "";
-        String[] fields;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the filename: ");
+        String fileName = input.next();
+        java.io.File file = new java.io.File(fileName);
+        input = new Scanner(file);
+        //prepare file for scanning
+
         String headingLine;
-        String[] routersToBePatched = new String[10];
-        String[] routersToNotBePatched = new String[10];
-        int invalidRouterCount = 0;
-        int validRouterCount = 0;
+        //stores the first line which contains heading for the routers
+        String currentLine = "";
+        int maxRouters = 10;
+        //end index for allRouters array... can be increased if array is full
+        String[] allRouters = new String[maxRouters];
+        //contains every router read from the CSV file
         int totalRouterCount = 0;
+        //initialise variables
 
         headingLine = input.nextLine();
-        System.out.println("headingLine = " + headingLine + "\n\n");
-
+        //discard the first line which contains the name for the fields of the router 
         while (input.hasNext()) {
             currentLine = input.nextLine();
-            isDuplicateHost = false;
-            isDuplicateIP = false;
-            patched = false;
-            invalidOS = false;
-
-            fields = currentLine.split(",");
-            //System.out.println("currentLine = " + currentLine);
-            for (int i = 0; i < fields.length; i++) {
-                //   System.out.println(fields[i]);
-            }
-            System.out.println("\n");
-            if (fields[2].equals("yes")) {
-                patched = true;
-                //dont patch
+            if ((currentLine == null) || (currentLine.trim().length() == 0)) {
+                //if the current line is empty or null discard it 
             } else {
-                patched = false;
+                allRouters[totalRouterCount] = currentLine;
+                totalRouterCount++;
+                //store the line (router) in an array (of routers)
             }
 
-            //check patched
-            if (Double.parseDouble(fields[3]) < 12) {
-                invalidOS = true;
-                //dont patch
-            } else {
-                invalidOS = false;
+            if (totalRouterCount == maxRouters) {
+                String[] tempRouters = new String[(maxRouters * 2)];
+                System.arraycopy(allRouters, 0, tempRouters, 0, totalRouterCount);
+                allRouters = tempRouters;
+                maxRouters = maxRouters * 2;
             }
-            //check OS
-
-            if (isDuplicateHost || isDuplicateIP || patched || invalidOS) {
-                System.out.println("The router below is invalid");
-                //if any of the above result in true, then it should not be patched
-                // System.out.println("The current INVALID router being checked is \n" + currentLine);
-                routersToNotBePatched[invalidRouterCount] = currentLine;
-                invalidRouterCount++;
-
-            } else {
-
-                System.out.println("The router below is valid");
-                //it should be patched
-                // System.out.println("The current VALID router: \n" + currentLine);
-                routersToBePatched[validRouterCount] = currentLine;
-                validRouterCount++;
-            }
-            totalRouterCount++;
-            System.out.println(totalRouterCount);
-
-            System.out.println("Current Router = " + currentLine);
-            System.out.println("dup host  " + isDuplicateHost);
-            System.out.println("dup IP    " + isDuplicateIP);
-            System.out.println("patched?  " + patched);
-            System.out.println("OS < 12?  " + invalidOS);
-            System.out.println("\n\n");
+            //if array gets full, copy the array to a new array with its size doubled. 
         }
 
-        System.out.println(
-                "\n\n\n THE RESULTS: \n\n valid router: " + validRouterCount);
+        String[] noDuplicateRouters = removeDuplicates(allRouters, totalRouterCount);
+        //the chain of functions returns an array with routers that do not share a hostname or ip address with any other routers
+        ArrayList<String> routerArrayList = new ArrayList<String>(Arrays.asList(noDuplicateRouters));
+        //converted to an array list 
+        routerArrayList = removeRemainingInvalidRouters(routerArrayList);
+        //removes routers which have are already patched, or have an OS > 12
+        printFinalRouters(routerArrayList);
+        //prints a list of routers that can be patched 
+    }
 
-        for (int i = 0;
-                i < validRouterCount;
-                i++) {
-            System.out.println("Router " + i + " = " + routersToBePatched[i]);
+    public static void printFinalRouters(ArrayList<String> arrList) {
+        String resultString = "";
+        for (String router : arrList) {
+            resultString = "";
+            ArrayList<String> fields = new ArrayList<String>(Arrays.asList(router.split(",")));
+
+            for (int i = 0; i < fields.size(); i++) {
+                switch (i) {
+                    case 0:
+                        resultString += fields.get(i);
+                        break;
+                    case 1:
+                        resultString += (" (" + fields.get(i) + "), ");
+                        break;
+                    case 2: //do not modify the patched field of the router
+                        break;
+                    case 3:
+                        resultString += ("OS version " + fields.get(i));
+                        break;
+                    case 4:
+                        resultString += " [" + fields.get(i) + "]";
+                } //modifies the necassary router field to match the expected output
+            }
+            System.out.println(resultString);
+            //prints each valid router
         }
+    }
 
-        System.out.println(
-                " \n\n invalid router: " + invalidRouterCount);
-
-        for (int i = 0;
-                i < invalidRouterCount;
-                i++) {
-            System.out.println("Router " + i + " = " + routersToNotBePatched[i]);
+    public static ArrayList<String> removeRemainingInvalidRouters(ArrayList<String> routers) {
+        //this function removes routers which have an OS < 12 and those which are already patched
+        ArrayList<String> invalidRouters = new ArrayList<String>();
+        //will store the routers that should not be patched
+        for (String router : routers) {
+            String[] fields = router.split(",");
+            //split each router string into an array of 5 fields
+            if ((Double.parseDouble(fields[3]) < 12) || (fields[2].equalsIgnoreCase("yes"))) {
+                //if the router has already been patched (field[2] = yes) or the OS (field[3]) < 12
+                invalidRouters.add(router);
+                //store the router in a separate arraylist
+            }
         }
-        System.out.println("\n\n\n");
-        checkDuplicateHostName(routersToBePatched, validRouterCount);
+        routers.removeAll(invalidRouters);
+        //remove all invalid routers from the main router array list
+        return routers;
+    }
+
+    public static String[] removeDuplicates(String[] array, int endIndex) {
+        //more appropriate name for the chain of functions which will effectively remove all routers which share a hostname or IP address
+        //with any other routers.
+        return buildBooleanArray(array, endIndex);
+    }
+
+    public static String[] buildBooleanArray(String[] array, int endIndex) {
+        //This function will check the array and inspect the fields in index 0 (hostname) and 1(ip address)
+        //if any routers share their hosntame or ip address
+        //then the positions representing those routers in the boolean area will be set to true (marking it for eventual removal)
+        boolean[] duplicates = new boolean[endIndex];
+        for (int i = 0; i < endIndex; i++) {
+            String[] fields = array[i].split(",");
+            for (int j = i + 1; j < endIndex; j++) {
+                String[] fields2 = array[j].split(",");
+                if ((fields[0].equalsIgnoreCase(fields2[0])) || (fields[1].equalsIgnoreCase(fields2[1]))) {
+                    //if hostname (field[0]) or ip (field[1]) of any Two routers are identical
+                    duplicates[i] = true;
+                    duplicates[j] = true;
+                    //the corresponding index in the boolean array is marked as true
+                }
+            }
+        }
+        return removePadding(duplicates, array, endIndex);
+    }
+
+    public static String[] removePadding(boolean[] booleanArray, String[] routers, int endIndex) {
+        //this function removes the preceeding true values(padding of trues) before the first false in the boolean array 
+        //these changes are also represented in the routers array
+        int numberOfPaddingElements = 0;
+        for (int i = 0; i < endIndex; i++) {
+            if (!booleanArray[i]) {
+                //locates the first false in the boolean array 
+                for (int j = i; j < endIndex; j++) {
+                    booleanArray[j - i] = booleanArray[j];
+                    //removes all the 'true' elements(the padding) preceeding the first 'false' 
+                    routers[j - i] = routers[j];
+                    //does the same for the array of routers
+                }
+                numberOfPaddingElements = i;
+                //stores the index of the first false (in its original position). 
+                i = endIndex;
+                //to exit the loop
+            }
+        }
+        endIndex -= numberOfPaddingElements;
+        //end index with number of padding elements removed.
+        return actualRemoval(routers, booleanArray, endIndex);
     }
 
     public static String[] actualRemoval(String[] duplicatesArray, boolean[] booleanArray, int endIndex) {
-        boolean duplicatesFound = false;
-        System.out.println("after removing the padding /before removing the duplicates");
-        printArrays(duplicatesArray, booleanArray, endIndex);
-        for (int i = 0; i < endIndex; i++) {
-            if (booleanArray[i]) {
-                duplicatesFound = true;
-                for (int j = i + 1; j < endIndex; j++) {
-                    booleanArray[j - 1] = booleanArray[j];
-                    duplicatesArray[j - 1] = duplicatesArray[j];
+        boolean duplicatesFound = true;
+        //required to enter the while loop 
+        while (duplicatesFound) {
+            duplicatesFound = false;
+            //set to false, will be set to true, if a duplicate is found, thus requiring another parse of the while loop
+            for (int i = 0; i < endIndex; i++) {
+                if (booleanArray[i]) {
+                    duplicatesFound = true;
+                    for (int j = i + 1; j < endIndex; j++) {
+                        booleanArray[j - 1] = booleanArray[j];
+                        duplicatesArray[j - 1] = duplicatesArray[j];
+                    }
+                    endIndex = endIndex - 1;
                 }
-                endIndex = endIndex - 1;
             }
         }
-        if (duplicatesFound) {
-            System.out.println("temp pass may need to remove more dupes");
-            printArrays(duplicatesArray, booleanArray, endIndex);
-            actualRemoval(duplicatesArray, booleanArray, endIndex);
-        } else {
-            System.out.println("After removing the duplicates");
-            //printArrays(duplicatesArray, booleanArray, endIndex);
-        }
-        System.out.println("Actual Removal endinded = " + endIndex);
+        //end while
+        //while loop has been exited, if the array contains false values 
+        //its accompanying array contains routers which do not share a hostname or IP address with any other router
         return trimRouterArray(duplicatesArray, endIndex);
     }
 
-    public static void checkDuplicateHostName(String[] array, int endIndex) {
-
-        //This function will recursively check for duplicates, until all are exterminated,
-        //Will do a final pass, if no duplicates are found (using the flag). Then the final list of routers has been determined
-        int checkingIndex = 0;
-        boolean duplicatesFound = false;
-        for (int i = 0; i < endIndex; i++) {
-            System.out.println(array[i]);
-        }
-
-        System.out.println("\n\n");
-        for (int i = 0; i < endIndex; i++) {
-            checkingIndex = i + 1;
-
-            String[] fields = array[i].split(",");
-            for (int j = checkingIndex; j < endIndex; j++) {
-                String[] fields2 = array[j].split(",");
-
-                if (fields[0].equalsIgnoreCase(fields2[0])) {
-                    System.out.println(fields[0] + " and " + fields2[0] + " are the same");
-                    System.out.println("I = " + i + " j = " + j);
-
-                    for (int k = j + 1; k < endIndex; k++) {
-                        array[k - 1] = array[k];
-                    }
-
-                    for (int k = i + 1; k < endIndex; k++) {
-                        array[k - 1] = array[k];
-                    }
-
-                    endIndex = endIndex - 2;
-                    duplicatesFound = true;
-                }
-
-                System.out.println("\n\n"); //checking intermediary array
-                if (fields[1].equalsIgnoreCase(fields2[1])) {
-
-                    System.out.println(fields[1] + " and " + fields2[1] + " are the same");
-
-                    System.out.println("I = " + i + " j = " + j);
-
-                    for (int k = j + 1; k < endIndex; k++) {
-                        array[k - 1] = array[k];
-                    }
-
-                    for (int k = i + 1; k < endIndex; k++) {
-                        array[k - 1] = array[k];
-                    }
-                    endIndex = endIndex - 2;
-                    duplicatesFound = true;
-                }
-            }
-        }
-        System.out.println("final array");
-        for (int i = 0; i < endIndex; i++) {
-            System.out.println(array[i]);
-        }
-
-        if (duplicatesFound) {
-            checkDuplicateHostName(array, endIndex);
-        } else {
-            //do nothing
-        }
-    }
-
     public static String[] trimRouterArray(String[] routerArray, int endIndex) {
-        System.out.println("Trim router was called");
-        System.out.println("FINAL END INDEX = " + endIndex);
+        //this function transfers the array to the necessary size (COMMENT MORE CLEARER)
         String[] newRouterArray = new String[endIndex];
         System.arraycopy(routerArray, 0, newRouterArray, 0, endIndex);
-        /* for (int i = 0; i < newRouterArray.length; i++) {
-            System.out.println(newRouterArray[i]);
-        }*/
         return newRouterArray;
     }
 
@@ -210,5 +190,4 @@ public class BTRouterPatch {
         }
         System.out.println("\n\n");
     }
-
 }
